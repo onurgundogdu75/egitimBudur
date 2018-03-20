48ca44851fa3b17cddc2ffab8f0af314cd49ea08
onurgundogdu75 <onur.gundogdu1@ogr.sakarya.edu.tr>
2018-03-20 07:10:47 +0300

    ilk gün

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+    <header>
+        <nav className="navbar navbar-expand-lg navbar-light container">
+            Eğitim Budur
+        </nav>
+    </header>
+  )
+}
+
+export default Header
