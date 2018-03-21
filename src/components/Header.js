e6209587feead28a671970e6a0ed43a67c6b17ec
onurgundogdu75 <onur.gundogdu1@ogr.sakarya.edu.tr>
2018-03-21 08:14:37 +0300

    header düzenlemesi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Header = () => {
   return (
 
     <header>
-        <nav className="navbar navbar-expand-lg navbar-light container">
-            Eğitim Budur
-        </nav>
+      <nav className='navbar navbar-expand-lg navbar-light container justify-content-between'>
+        <a className='navbar-brand' href='#'>Eğitim Budur</a>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <Link to='/' className='nav-link' activeClassName='active'>Ana Sayfa</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='' className='nav-link' activeClassName='active'>Hakkımızda</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='' className='nav-link' activeClassName='active'>İletişim</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
+/* import { IndexLink, Link } from 'react-router' */
 import PropTypes from 'prop-types'
-import './PageLayout.scss'
+import './PageLayout.sass'
 
 import Header from '../../components/Header'
 
