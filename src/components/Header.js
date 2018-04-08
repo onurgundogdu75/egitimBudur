fb369ce11934d13692de1b25b2dd711983aedbb7
onurgundogdu75 <onur.gundogdu1@ogr.sakarya.edu.tr>
2018-04-08 20:40:38 +0300

    header mobil

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const Header = () => {
-  return (
+class Header extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      isNavOpen: false
+    }
+  }
 
-    <header>
-      <nav className='navbar navbar-expand-lg navbar-light container justify-content-between'>
-        <a className='navbar-brand' href='#'>Eğitim Budur</a>
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <Link to='/' className='nav-link' activeClassName='active'>Ana Sayfa</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='' className='nav-link' activeClassName='active'>Hakkımızda</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='' className='nav-link' activeClassName='active'>İletişim</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+  buttonClicked () {
+    this.setState = ({
+      isNavOpen: true
+    })
+  }
+
+  render () {
+    return (
+
+      <header>
+        <nav className='navbar navbar-expand-lg navbar-light container justify-content-between'>
+          <a className='navbar-brand' href='#'>Eğitim Budur</a>
+          <button className='navbar-toggler' type='button' onClick={this.buttonClicked.bind(this)}>
+            <span className='navbar-toggler-icon' />
+          </button>
+          <div className='navbar-collapse' style={{ display: this.state.isNavOpen ? 'block' : 'none' }}>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <Link to='/' className='nav-link' activeClassName='active'>Ana Sayfa</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='' className='nav-link' activeClassName='active'>Hakkımızda</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='' className='nav-link' activeClassName='active'>İletişim</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    )
+  }
 }
 
 export default Header
